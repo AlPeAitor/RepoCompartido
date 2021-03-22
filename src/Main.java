1e18e49778ea56ce993336c442cf1e808d506616
Sergio <blazquezsergio4@gmail.com>
2021-03-22 14:15:48 +0100

    Commit Sergio2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String [] args){
         System.out.println("Hey");
+        System.out.println("Hola");
+        System.out.println("¿Hola,qué tal?");
+        System.out.println("Adios");
+
         System.out.println("Segundo commit");
     }
 }
